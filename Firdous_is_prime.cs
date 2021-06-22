ac6a06662f0bddfc53fc6472d766164aa0229ee3
firdoussamreen <firdoussamreen56@gmail.com>
2021-06-21 19:54:44 -0500

    submission2

--- Firdous_is_prime.cs
+++ Firdous_is_prime.cs
@@ -2,14 +2,14 @@ public class PrimeNumberChecker
 {
   public static bool isPrime(int n)
   {
-    if(n == 2 || n == 3)
-      return true;
-    if(n % 2 == 0 || n % 3 ==0|| n == 5 || n == 1)
-      return false;
-    int limit = (int)System.Math.Ceiling(  System.Math.Sqrt(n));
-    for(int i = 6; i < limit; i+=6)
-      if(n % (i + 1) == 0|| n%(i - 1) == 0)
+    if(n == 2 || n == 3) return true;
+      
+    if(n % 2 == 0 || n % 3 ==0|| n == 5 || n == 1) return false;
+
+    for(int i = 6; i < (int)System.Math.Ceiling(System.Math.Sqrt(n)); i+=6)
+    if(n % (i + 1) == 0|| n%(i - 1) == 0) 
         return false;
+
     return true;
   }
 
